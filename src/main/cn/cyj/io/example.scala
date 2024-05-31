d36554f2c6ecbf4c45718d54529756e3e06b18a0
Cyj <2677575051@qq.com>
2024-05-31 20:58:27 +0800

    mod

--- src/main/cn/cyj/io/example.scala
+++ src/main/cn/cyj/io/example.scala
@@ -8,25 +8,25 @@ object example {
 
     case class Student(var name: String, var chinese: Int, var math: Int, var english: Int){
 
-        def getTotalScore() = chinese + math + english
+        def getTotalScore: Int = chinese + math + english
     }
 
     def main(args: Array[String]): Unit = {
-        var source = Source.fromFile("./data/student.txt")
+        val source = Source.fromFile("./data/student.txt")
         val stuArray = source.getLines().map(_.split("\\s+"))
         val stuList = new ListBuffer[Student]()
         for(stu <- stuArray) {
-            stuList += new Student(stu(0), stu(1).toInt, stu(2).toInt, stu(3).toInt)
+            stuList += Student(stu(0), stu(1).toInt, stu(2).toInt, stu(3).toInt)
         }
         //println(stuList)
-        val sortList = stuList.sortBy(_.getTotalScore()).reverse.toList
+        val sortList = stuList.sortBy(_.getTotalScore).reverse.toList
         //println(sortList)
         //           高效的字符输出流      普通字符输出流
         val bw = new BufferedWriter(new FileWriter("./data/stu.txt"))
         bw.write("姓名 语文成绩 数学成绩 英语成绩 总成绩")
         bw.newLine()
         for(s <- sortList) {
-            bw.write(s"${s.name} ${s.chinese}     ${s.math}     ${s.english}     ${s.getTotalScore()}")
+            bw.write(s"${s.name} ${s.chinese}     ${s.math}     ${s.english}     ${s.getTotalScore}")
             bw.newLine()
         }
         bw.close()
