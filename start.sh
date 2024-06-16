e43570aac30ca8f51ffccadd247afa0102a06144
Cyj <2677575051@qq.com>
2024-06-16 20:29:45 +0800

    考核题

--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
-echo "执行的文件名：$1";
-
 PORT=9001
-# 使用 lsof 查找并杀死进程
 PID=$(lsof -t -i :$PORT)
 if [ -n "$PID" ]; then
   echo "Killing process $PID on port $PORT"
@@ -12,5 +9,5 @@ fi
 unzip $1
 mv squarerootapi-1.0-SNAPSHOT/*  /opt/square-root-api
 cp /opt/square-root-api/conf/application.conf /opt/square-root-api/application_prod.conf
-nohup /opt/square-root-api/bin/squarerootapi -J-server -J-Xms256M -J-Xmx512M -Dconfig.file=application_prod.conf -DapplyEvolutions.default=false -Dhttp.port=9001 &
+nohup /opt/square-root-api/bin/squarerootapi -J-server -J-Xms256M -J-Xmx512M -J-XX:+UseG1GC -Dconfig.file=application_prod.conf -DapplyEvolutions.default=false -Dhttp.port=9001 &
 
